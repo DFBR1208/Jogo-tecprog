193fe37c40a8ffc71596e5aada4506454ecb7342
Diogo Corrêa do Nascimento <digocnascimento@gmail.com>
2026-05-19 02:01:04 -0300

    Mover, cores, formas

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Primeiro Teste");
-    sf::RectangleShape jogador(sf::Vector2f(50.f, 50.f));
-    jogador.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "");
+    window.setFramerateLimit(60);
+
+    sf::CircleShape jogador(75.f);
+    jogador.setFillColor(sf::Color::Red);
+    jogador.setPosition(275.f, 275.f); 
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) window.close();
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
         }
+        float velocidade = 5.0f; 
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            jogador.move(velocidade, 0.f); 
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            jogador.move(-velocidade, 0.f); 
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            jogador.move(0.f, -velocidade); 
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            jogador.move(0.f, velocidade);
+        }
+
         window.clear();
         window.draw(jogador);
         window.display();
